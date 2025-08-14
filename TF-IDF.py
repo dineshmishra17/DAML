e33cf00f3b414bc4bf5762bee24bcdc11cc5a60c
dineshmishra17 <dineshmishra1721@gmail.com>
2025-08-14 15:59:50 +0530

    Add files via upload

---
+++ TF-IDF.py
@@ -0,0 +1,25 @@
+
+# PRACTICAL 4 : TFIDF
+import nltk
+nltk.download('stopwords')
+# TF-IDF
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+#pip install scikit-learn
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+stopwords = set(stopwords.words('english'))
+ps = PorterStemmer()
+vector = TfidfVectorizer()
+
+documents= [ "apple boy cat", "apple cat dog", "dog egg fan" ]
+
+preprocessed = []
+for doc in documents:
+    tokenization = word_tokenize(doc)
+    stop = [ps.stem(word) for word in tokenization if word not in stopwords]
+    preprocessed.append(" ".join(stop))
+print(preprocessed)
+word_score = vector.fit_transform(preprocessed)
+print(word_score)
